440eb1463278ce86efb7648b170dc20efd8dd47f
XP <maivutan@gmail.com>
2020-03-05 13:14:51 +0800

    updated list of prohibited words, re-ordered AutoModerator's filtering rules, censored login credentials
    also updated InstaMod.ini which was done by u/shimmyjimmy97

---
+++ Delete Duplicate Comments.py
@@ -0,0 +1,88 @@
+import praw
+import json
+from time import time
+
+
+client_id = ''
+client_secret = ''
+reddit_user = ''
+reddit_pass = ''
+user_agent = 'Duplicate Remover (by /u/impshum)'
+
+target_subreddit = 'HomeworkHelp'
+
+send_message = 0
+delete_post = 0
+
+reddit = praw.Reddit(client_id=client_id,
+                     client_secret=client_secret,
+                     user_agent=user_agent,
+                     username=reddit_user,
+                     password=reddit_pass)
+
+
+def get_threads():
+    with open('threads.txt') as f:
+        data = f.readlines()
+        data = [x.strip() for x in data]
+    return data
+
+
+def do_db(token, id):
+    with open('data.json') as f:
+        data = json.load(f)
+    gotcha = data.get(token)
+    if gotcha and gotcha != id:
+        return False
+    else:
+        data.update({token: id})
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
+        return True
+
+
+def stream():
+    print('\nStarting the stream\n')
+    start = time()
+    for comment in reddit.subreddit(target_subreddit).stream.comments():
+        if comment.created_utc > start:
+            id = comment.id
+            user = comment.author.name
+            body = comment.body.replace('*', '').replace('\n', '').strip().lower()
+            special_threads = get_threads()
+            if comment.submission in special_threads:
+                if do_db(body, id):
+                    print(f'New comment: {body}')
+                else:
+                    response(comment, user, body)
+
+
+def history():
+    print('\nTrawling through old comments\n')
+    special_threads = get_threads()
+    for thread in special_threads:
+        submission = reddit.submission(id=thread)
+        submission.comments.replace_more(limit=None)
+        for comment in submission.comments.list():
+            id = comment.id
+            if comment.author:
+                user = comment.author.name
+                body = comment.body.replace('*', '').replace('\n', '').strip().lower()
+                if do_db(body, id):
+                    print(f'New comment: {body}')
+                else:
+                    response(comment, user, body)
+
+
+def response(comment, user, body):
+    if send_message:
+        msg = f'Your comment has been removed from {target_subreddit} as it has already been posted. Sorry.'
+        reddit.redditor(user).message('Oh noes!', msg)
+    if delete_post:
+        comment.mod.remove()
+        #comment.delete()
+        print(f'Deleted duplicate: {body}')
+
+
+history()
+stream()
